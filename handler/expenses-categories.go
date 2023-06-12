9eec00573ddd201355666d469e10849082c43750
Luis Souza <luisotavioldesouza1+1@gmail.com>
2023-06-12 18:36:46 -0300

    chore: finished expenses categories endpoints

--- handler/expenses-categories.go
+++ handler/expenses-categories.go
@@ -1,15 +1,62 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/luis-souza-dev/fintracker-be/database"
+	"github.com/luis-souza-dev/fintracker-be/models"
+)
+
+type ExpensesCategoriesQuery struct {
+	Name string `query:"name"`
+}
 
 func GetExpensesCategories(c *fiber.Ctx) error {
-	return c.Status(200).SendString("expenses categories works")
+	result := &[]models.ExpensesCategories{}
+	query := &ExpensesCategoriesQuery{}
+	if err := c.QueryParser(query); err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(fmt.Sprintf("query err%v", err))
+	}
+	
+	database.DB.Where(&models.ExpensesCategories{
+		Name: query.Name,
+	}).Find(result)
+
+	jsonval, err := json.Marshal(result);
+	if err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(fmt.Sprintf("json err %v", err))
+	}
+	return c.Status(200).SendString(string(jsonval))
+
 }
 
 func CreateExpensesCategories(c *fiber.Ctx) error {
-	return c.Status(200).SendString("expenses categories works")
+	expenseCategory := &models.ExpensesCategories{}
+	if err := c.BodyParser(expenseCategory); err != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(fmt.Sprintf("body err%v", err))
+	}
+	if expenseCategory.Name == "" {
+		return c.Status(fiber.ErrBadRequest.Code).SendString("Missing name")
+	}
+	res := database.DB.Create(expenseCategory)
+	if res.Error != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString(fmt.Sprintf("creation err%v", res.Error))
+	}
+
+	return c.Status(fiber.StatusCreated).SendString(fmt.Sprint(expenseCategory.ID))
 }
 
 func DeleteExpensesCategories(c *fiber.Ctx) error {
-	return c.Status(200).SendString("expenses categories works")
+	id := c.Params("id");
+	if id == "" {
+		return c.Status(fiber.ErrBadRequest.Code).SendString("Missing id")
+	}
+	res := database.DB.Delete(&models.ExpensesCategories{}, id)
+
+	if res.Error != nil {
+		return c.Status(fiber.ErrBadRequest.Code).SendString("Deletion err")
+	}
+	return c.Status(200).SendString("done")
 }
